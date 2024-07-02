7c53e6f5da67eeabb51b984885ebe24f511b6173
Matt Anderson <matta9001@gmail.com>
2024-07-02 07:04:12 -0500

    Basic login with email, hash password with bcrypt, store in sqlite, compare hash for login

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/mannders00/ezauth
 
 go 1.22.4
 
-require modernc.org/sqlite v1.30.1
+require (
+	golang.org/x/crypto v0.24.0
+	modernc.org/sqlite v1.30.1
+)
 
 require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
@@ -11,7 +14,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 	modernc.org/gc/v3 v3.0.0-20240107210532-573471604cb6 // indirect
 	modernc.org/libc v1.52.1 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
--- main.go
+++ main.go
@@ -2,22 +2,38 @@ package main
 
 import (
 	"database/sql"
+	"embed"
 	"fmt"
-	_ "modernc.org/sqlite"
+	"html/template"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+	_ "modernc.org/sqlite"
 )
+
+//go:embed static/*
+var fs embed.FS
+var db *sql.DB
 
 func main() {
 
-	db, err := sql.Open("sqlite", "file:data.db")
+	// load templates
+	tmpl, err := template.ParseFS(fs, "static/templates/*")
+	if err != nil {
+		panic(err)
+	}
+
+	// setup sqlite
+	db, err = sql.Open("sqlite", "file:data.db")
 	if err != nil {
 		panic(err)
 	}
 
 	createTableSQL := `
-	Create Table if not exists users (
-		id integer primary key autoincrement,
-		name text not null
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
 	);`
 
 	_, err = db.Exec(createTableSQL)
@@ -29,7 +45,68 @@ func main() {
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello")
 	})
+	mux.HandleFunc("POST /register", registerHandler)
+	mux.HandleFunc("GET /register", renderTemplate(tmpl, "register.html", nil))
+
+	mux.HandleFunc("POST /login", loginHandler)
+	mux.HandleFunc("GET /login", renderTemplate(tmpl, "login.html", nil))
+
+	fmt.Println("Listening on :8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		panic(err)
 	}
+}
+
+func renderTemplate(tmpl *template.Template, view string, data interface{}) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tmpl.ExecuteTemplate(w, view, data)
+	}
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	password_confirm := r.FormValue("password_confirm")
+
+	if password != password_confirm {
+		http.Error(w, "Passwords don't match", http.StatusBadRequest)
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_, err = db.Exec("INSERT INTO users (email, password) VALUES ($1, $2)", email, string(hashedPassword))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("User created")
+	w.WriteHeader(http.StatusCreated)
+
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	var storedPassword string
+	err := db.QueryRow("SELECT password FROM users WHERE email=($1)", email).Scan(&storedPassword)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(storedPassword), []byte(password))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	fmt.Println("User verified")
+	w.WriteHeader(http.StatusCreated)
 }
